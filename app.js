397418b7bc9aa952ef9f311c720644bc4074f495
Ramiro-isla <ramiroisla11@gmail.com>
2022-12-01 19:01:14 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+let divTriangle = document.querySelector("#triangle");
+let divSquare = document.querySelector("#square");
+let divCircle = document.querySelector("#circle");
+
+function removeCircle() {
+  divCircle.remove();
+}
+function removeTriangle() {
+  divTriangle.remove();
+}
+function removeSquare() {
+  divSquare.remove();
+}
+function reload() {
+    location. reload()
+}
